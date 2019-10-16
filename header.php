3c15b39ae35d0b7f4df4c915522a84a448264911
mathiasjor911 <54143780+mathiasjor911@users.noreply.github.com>
2019-10-16 23:20:16 +0200

    Skiftet filer til PHP og arbejder med dem i XAMPP

---
+++ header.php
@@ -0,0 +1,55 @@
+<meta charset="utf-8" />
+<meta name="author" content="Mathias Jørgensen" />
+<meta name="copyright" content="Mathias Jørgensen" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Normalize CSS -->
+<link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Catamaran:300|Roboto+Mono&display=swap" rel="stylesheet">
+
+<!-- Feather Icons -->
+<script src="https://unpkg.com/feather-icons"></script>
+
+<!-- Egen CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="wrapper">
+            <div class="logo">
+                <a href="/">Mathias Jørgensen</a>
+            </div>
+            <div class="links">
+                <ul>
+                    <li>
+                        <a href="/portfolio.html">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="/foto.html">Foto</a>
+                    </li>
+                    <li>
+                        <a href="/om.html">Om mig</a>
+                    </li>
+                </ul>
+                <div class="social-links">
+                    <a href="#">
+                        <i data-feather="mail"></i>
+                    </a>
+                    <a href="#">
+                        <i data-feather="instagram"></i>
+                    </a>
+                    <a href="#">
+                        <i data-feather="github"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="nav-menu">
+                <i data-feather="menu"></i>
+            </div>
+        </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,18 @@
+<title>Forside - Mathias Jørgensen</title>
+<meta name="description" content="" />
+
+<?php
+  $path = $_SERVER['DOCUMENT_ROOT'];
+  $path .= '/header.php';
+  include_once($path);
+  ?>    
+    <section>
+
+    </section>
+
+    <?php
+  $path = $_SERVER['DOCUMENT_ROOT'];
+  $path .= '/footer.php';
+  include_once($path);
+  ?>   
+   
