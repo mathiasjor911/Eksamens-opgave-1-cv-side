12eb86c3dd88a25b347048f29b9510a497d20e42
mathiasjor911 <54143780+mathiasjor911@users.noreply.github.com>
2019-10-28 14:02:01 +0100

    Tilføjelse af Cookie banner

--- footer.php
+++ footer.php
@@ -20,4 +20,17 @@ $(".nav-menu").click(function(){
 <script>
     feather.replace()
 </script>
+
+<script type="text/javascript" id="cookieinfo"
+	src="//cookieinfoscript.com/js/cookieinfo.min.js"
+  data-message="Denne side gør brug af cookies til statistik. Ved yderligere brug af siden, acceptere du brug af cookies."
+  data-linkmsg="Læs mere om cookies her"
+  data-close-text="Forstået"
+  data-divlink="white"
+  data-divlinkbg="#93C088"
+  data-fg="#f5f5f5"
+  data-font-family="'Roboto Mono', sans-serif"
+  data-link="#93C088"
+  data-bg="#24262B">
+</script>
 </body>
